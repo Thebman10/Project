6938dba7a93341bf56d83e61caece8d2fa2d2017
Thebman10 <baasimsohail10@gmail.com>
2024-12-30 23:29:26 -0500

    Update Bioactivity_predictor_app.py

--- Bioactivity_predictor_app.py
+++ Bioactivity_predictor_app.py
@@ -19,7 +19,7 @@ def check_java_installation():
 # Molecular descriptor calculator
 def desc_calc():
     # Performs the descriptor calculation
-    bashCommand = "java -Xms2G -Xmx2G -Djava.awt.headless=true -jar ./PaDEL -Descriptor/PaDEL-Descriptor.jar -removesalt -standardizenitro -fingerprints -descriptortypes ./PaDEL -Descriptor/PubchemFingerprinter.xml -dir ./ -file descriptors_output.csv"
+    bashCommand = "java -Xms2G -Xmx2G -Djava.awt.headless=true -jar ./PaDEL -Descriptor/PaDEL\ -Descriptor.jar -removesalt -standardizenitro -fingerprints -descriptortypes ./PaDEL\ -Descriptor/PubchemFingerprinter.xml -dir ./ -file descriptors_output.csv"
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
     os.remove('molecule.smi')  # Clean up temporary file
